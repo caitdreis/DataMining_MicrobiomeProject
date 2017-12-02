e4eb34bb5e22d26977658306b616d084e09050b0
Cait Dreisbach <cnd2y@virginia.edu>
2017-12-02 10:44:53 -0500

    Update MicrobiomeProject.R

--- MicrobiomeProject.R
+++ MicrobiomeProject.R
@@ -6,3 +6,17 @@ library(tidyverse) # Load the core tidyverse packages: ggplot2, tibble,
 setwd("~/Documents/GitHub/DataMining_MicrobiomeProject")
 
 microbiome <- read.csv("MicrobiomeWithMetadata.csv", encoding = 'utf-8', stringsAsFactors = FALSE)
+
+#----------------- Grading Criteria
+#Data exploration
+#Data cleaning (missing values, outliers, etc.)
+#Rationale for the selected statistical modeling methods
+#Correct implementation and use of statistical modeling methods
+#Appropriate model selection approach (train/test, cross-validation, etc.)
+#Thoroughly documented code (R comments)
+#Appropriate use of functions to reduce code complexity and redundancy
+#Writing quality for final report, evaluated in terms of conformance to process outline, level of detail, and correctness.
+
+#----------------- Data Cleaning
+
+#----------------- Data Exploration
